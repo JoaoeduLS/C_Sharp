79c034a9147d17f7b7f3859b50546cc7780173b4
JoaoeduLS <joaoeduardo.lsilva@gmail.com>
2023-07-28 02:08:34 -0300

    Adicionar arquivos de projeto.

---
+++ Aprendizado de C#/JogoDosNumeros.cs
@@ -0,0 +1,74 @@
+﻿
+    internal class JogoDosNumeros
+    {
+
+    void JogoDeNumeroAleatorio()
+    {
+        ///titulo
+        Console.WriteLine(@"
+    
+░░░░░██╗░█████╗░░██████╗░░█████╗░  ██████╗░░█████╗░░██████╗
+░░░░░██║██╔══██╗██╔════╝░██╔══██╗  ██╔══██╗██╔══██╗██╔════╝
+░░░░░██║██║░░██║██║░░██╗░██║░░██║  ██║░░██║██║░░██║╚█████╗░
+██╗░░██║██║░░██║██║░░╚██╗██║░░██║  ██║░░██║██║░░██║░╚═══██╗
+╚█████╔╝╚█████╔╝╚██████╔╝╚█████╔╝  ██████╔╝╚█████╔╝██████╔╝
+░╚════╝░░╚════╝░░╚═════╝░░╚════╝░  ╚═════╝░░╚════╝░╚═════╝░
+
+███╗░░██╗██╗░░░██╗███╗░░░███╗███████╗██████╗░░█████╗░░██████╗
+████╗░██║██║░░░██║████╗░████║██╔════╝██╔══██╗██╔══██╗██╔════╝
+██╔██╗██║██║░░░██║██╔████╔██║█████╗░░██████╔╝██║░░██║╚█████╗░
+██║╚████║██║░░░██║██║╚██╔╝██║██╔══╝░░██╔══██╗██║░░██║░╚═══██╗
+██║░╚███║╚██████╔╝██║░╚═╝░██║███████╗██║░░██║╚█████╔╝██████╔╝
+╚═╝░░╚══╝░╚═════╝░╚═╝░░░░░╚═╝╚══════╝╚═╝░░╚═╝░╚════╝░╚═════╝░
+");
+
+
+
+        ///Para numeros aleatorios ,
+        Random rand = new Random();
+        int numeroAleatorio = rand.Next(1, 20);
+
+
+        int resposta;
+
+
+        do
+        {
+            Console.WriteLine("Escolha um número:");
+            string numeroEscolhido = Console.ReadLine();
+
+
+            ///Nem sempre o int se repete e tem que esta no mesmo lugar.
+            resposta = int.Parse(numeroEscolhido);
+
+
+            if (resposta == numeroAleatorio)
+            {
+                Console.WriteLine("parabens voce ganho o jogo");
+            }
+            else if (resposta > numeroAleatorio)
+            {
+
+                Console.WriteLine("voce passou direto maninho vai mais devaga e diminui ai");
+            }
+            else if (resposta < numeroAleatorio)
+            {
+                Console.WriteLine("chegou quase la em maninho mais almenta isso");
+            }
+            else
+            {
+                Console.WriteLine("digita um numero ai");
+            }
+
+
+        } while (
+           ///enquanto for diferente a funcao vai se repetir.
+           resposta != numeroAleatorio);
+        {
+            Console.WriteLine("parabens manito voce passou");
+        }
+    }
+    JogoDeNumeroAleatorio();
+
+}
+
---
+++ Aprendizado de C#/Program.cs
@@ -0,0 +1,115 @@
+﻿string primeiraApresentacao = "Seja bem vindo ao MusicSharp";
+
+
+//Fazendo uma lista 
+List<string> Musicas = new List<string>();
+void ChamandoApresentacao()
+{
+
+    //Titulo do programa
+    Console.WriteLine(@"
+    
+███╗░░░███╗██╗░░░██╗░██████╗██╗░█████╗░░██████╗██╗░░██╗░█████╗░██████╗░
+████╗░████║██║░░░██║██╔════╝██║██╔══██╗██╔════╝██║░░██║██╔══██╗██╔══██╗
+██╔████╔██║██║░░░██║╚█████╗░██║██║░░╚═╝╚█████╗░███████║███████║██████╔╝
+██║╚██╔╝██║██║░░░██║░╚═══██╗██║██║░░██╗░╚═══██╗██╔══██║██╔══██║██╔═══╝░
+██║░╚═╝░██║╚██████╔╝██████╔╝██║╚█████╔╝██████╔╝██║░░██║██║░░██║██║░░░░░
+╚═╝░░░░░╚═╝░╚═════╝░╚═════╝░╚═╝░╚════╝░╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░░░░
+");
+    Console.WriteLine(primeiraApresentacao);
+}
+
+
+void Comandos()
+{
+
+    Console.WriteLine("\n1- Registro de musica");
+    Console.WriteLine("2- Mostra a musica");
+    Console.WriteLine("3- Avaliacao da musica");
+    Console.WriteLine("4- Autor da musica");
+    Console.WriteLine("5- Sair");
+
+
+
+
+    Console.Write("\nEscolha um das opcoes assima: ");
+
+
+    string opcaoEscolhida = Console.ReadLine();
+    int EscolhaDeComandoNumerica = int.Parse(opcaoEscolhida)!;
+
+   
+
+
+    switch (EscolhaDeComandoNumerica)
+    {
+        case 1:
+            RegistroDeMuscia();
+        break;
+        case 2:
+            MostraMusicas();
+            break;
+        default:
+            break;
+
+
+
+
+
+            void RegistroDeMuscia()
+            {
+               
+                    Console.Clear();
+                    Console.WriteLine("**********************");
+                    Console.WriteLine("Registro de musicas");
+                    Console.WriteLine("**********************\n");
+                    Console.Write("Digite a musica que deseja registrar: ");
+                    string nomeDamusica = Console.ReadLine()!;
+                    Musicas.Add(nomeDamusica);
+                    Console.WriteLine($"A musica {nomeDamusica} foi registrada com sucesso!");
+                    Thread.Sleep(2000);
+                    Console.Clear();
+                    Comandos();
+
+            }
+
+
+            void MostraMusicas()
+            {
+
+                Console.Clear();
+                Console.WriteLine("************************************");
+                Console.WriteLine("Exibindo todas as musicas registradas");
+                Console.WriteLine("************************************\n");
+
+                for (int i = 0; i < Musicas.Count; i++)
+                {
+                Console.WriteLine($"Banda: {Musicas[i]}");
+                }
+
+                /*
+                ///outra forma de fazer o for para o registro da musica
+                foreach (string nomedaMusica in Musicas)
+                {
+                    Console.WriteLine($"Banda: {nomedaMusica}");
+                }
+                */
+
+
+                Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+                Console.ReadKey();
+                Console.Clear();
+                Comandos();
+            }
+
+    }
+}
+
+
+
+
+ChamandoApresentacao();
+Comandos(); 
+
+
+
